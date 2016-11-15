ffa1f7e704cce1cf7d83129f4350db01bc279d82
Taras Mankovski <tarasm@gmail.com>
2016-11-15 16:56:44 -0500

    Fixed analytics code

--- config/environment.js
+++ config/environment.js
@@ -47,7 +47,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    ENV.APP.analytics = {
+    ENV.analytics = {
       integrations: [
         {
           name: 'GoogleAnalytics',
