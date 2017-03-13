d93bfbb57f92cef4b41db73171a712cfe85f9929
zinyando <brizdigital@gmail.com>
2017-03-13 09:11:54 +0200

    Change dates

--- app/fixtures.js
+++ app/fixtures.js
@@ -1,41 +1,30 @@
-import moment from 'moment';
-import Ember from 'ember';
+import moment from "moment";
+import Ember from "ember";
 
 const { guidFor } = Ember;
 
-const online = 'https://www.eventbrite.com/e/online-managing-async-with-rxjs-5-tickets-29383184897';
-const siliconValley = 'https://www.eventbrite.com/e/silicon-valley-managing-async-with-rxjs-5-tickets-29383246080';
+const online = "https://www.eventbrite.com/e/online-managing-async-with-rxjs-5-tickets-29383184897";
+const siliconValley = "https://www.eventbrite.com/e/silicon-valley-managing-async-with-rxjs-5-tickets-29383246080";
 
 export default {
   data: [].concat(
-    [
-      '2017-03-03 08:00:00',
-      '2017-06-23 08:00:00',
-      '2017-09-15 08:00:00'
-    ].map(
-      date => doc('event', {
-        date: iso(date),
-        price: 595,
-        location: 'Silicon Valley',
-        link: siliconValley
-      })
-    ),
-    [
-      '2017-04-13 08:00:00',
-      '2017-08-10 08:00:00'
-    ].map(
-      date => doc('event', {
-        date: iso(date),
-        price: 495,
-        location: 'Online',
-        link: online
-      })
-    )
+    ["2017-06-24 08:00:00", "2017-09-16 08:00:00"].map(date => doc("event", {
+      date: iso(date),
+      price: 595,
+      location: "Silicon Valley",
+      link: siliconValley
+    })),
+    ["2017-04-13 08:00:00", "2017-08-12 08:00:00"].map(date => doc("event", {
+      date: iso(date),
+      price: 495,
+      location: "Online",
+      link: online
+    }))
   )
 };
 
 function iso(date) {
-  return moment.tz(date, 'America/Los_Angeles').format();
+  return moment.tz(date, "America/Los_Angeles").format();
 }
 
 function doc(type, attributes) {
