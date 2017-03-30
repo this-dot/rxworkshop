211b8b4cec8579a21594317646f6f21b835c0d45
Lennex Zinyando <brizdigital@gmail.com>
2017-03-30 14:15:14 +0200

    Add recorded workshop (#15)
    Add recorded workshop

--- app/components/workshop-card.js
+++ app/components/workshop-card.js
@@ -1,5 +1,11 @@
 import Ember from 'ember';
+import moment from 'moment';
 
 export default Ember.Component.extend({
-  tagName: ''
+  viewable: 'Viewable Anytime',
+  tagName: '',
+  validDate: Ember.computed('model.date', function() {
+    let date = this.get('model.date');
+    return moment(date).isValid();
+  })
 });
