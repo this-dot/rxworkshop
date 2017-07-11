f7948f8b054cef2814b1b9ad1d2b3811d7a5514e
Taras Mankovski <tarasm@gmail.com>
2017-07-11 15:17:43 -0400

    Removed 8/12 workshop

--- app/fixtures.js
+++ app/fixtures.js
@@ -16,7 +16,7 @@ export default function() {
         location: 'Silicon Valley',
         link: siliconValley
       })),
-      ['2017-08-12 08:00:00', '2017-09-09 09:00:00'].map(date => doc('event', {
+      ['2017-09-09 09:00:00'].map(date => doc('event', {
         date: iso(date),
         price: 495,
         location: 'Online',
